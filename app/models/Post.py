6fc8e6719e9bf117c334792d1f945a1818418b95
samuel de jesus <samue@MacBook-Air.local>
2023-09-09 19:56:03 -0400

    added remaning api routes to create update and delete posts and comments also added authentication security

--- app/models/Post.py
+++ app/models/Post.py
@@ -2,22 +2,21 @@ from datetime import datetime
 from app.db import Base
 from .Vote import Vote
 from sqlalchemy.ext.hybrid import hybrid_property
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
-from sqlalchemy.orm import relationship, column_property
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, func
+from sqlalchemy.orm import relationship
 
 class Post(Base):
-  __tablename__ = 'posts'
-  id = Column(Integer, primary_key=True)
-  title = Column(String(100), nullable=False)
-  post_url = Column(String(100), nullable=False)
-  user_id = Column(Integer, ForeignKey('users.id'))
-  created_at = Column(DateTime, default=datetime.now)
-  updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-  user = relationship('User')
-  comments = relationship('Comment', cascade='all,delete')
-  votes = relationship('Vote', cascade='all,delete')
-#   vote_count = column_property(select([func.count(Vote.id)]).where(Vote.post_id == id))
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    post_url = Column(String(100), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+    user = relationship('User')
+    comments = relationship('Comment', cascade='all,delete')
+    votes = relationship('Vote', cascade='all,delete')
 
-@hybrid_property
-def vote_count(self):
- return len(self.votes)
+    @hybrid_property
+    def vote_count(self):
+        return len(self.votes)
